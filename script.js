5eb5d9a455fa167075268514396202827f09fe0e
silvakwan1 <123783319+silvakwan1@users.noreply.github.com>
2023-09-16 22:05:57 -0300

    finix

--- script.js
+++ script.js
@@ -6,50 +6,103 @@ botaoBuscar.addEventListener("click", verificador);
 
 input.addEventListener("keydown", (event) => {
     if (event.keyCode === 13) {
-        verificador()
-    }
+    verificador()
+}
 });
-    
+
 function verificador(){
-    let msn = verificar(input.value)
-    if(msn === undefined){
-        msn = "você não estar autorisado(a)"
-        renderDom(msn)
-        console.log("bom dia")
-    }
-    renderDom(msn)
-    console.log(verificar(input.value))
-}
+    renderDom()
 
-function renderDom(mensagem){
-    let paragrafo = document.createElement("p")
+    let minhaDiv = document.querySelector(".box-render");
+    let filhosDiv = minhaDiv.children;
 
-    paragrafo.textContent = mensagem
-    paragrafo.classList.add("text-render")
-    if(mensagem === "você não estar autorisado(a)"){
-        render.textContent = " "
-        paragrafo.classList.add("erro")
-        render.appendChild(paragrafo)
-    }else{
-        
-        render.textContent = " "
-        render.appendChild(paragrafo)
+    let i = 0;
+    while (i < filhosDiv.length) {
+        let filho = filhosDiv[i];
+
+        if (filho.textContent.trim() === "" || filho.innerHTML.trim() === "") {
+            filho.remove();
+        } else {
+            i++;
+        }
     }
 }
 
-function verificar(name) {
+function verificarName(name) {
     let nomeVerificado;
   
-    if (name === "jeni" || name === "gabi") {
-      nomeVerificado = "marques voce e perfeita maravilhosa receba essa galaxia de presente";
-    } else if (name === "lusia" || name === "bia") {
-      nomeVerificado = "perfeita maravilhosa receba essa galaxia de presente";
-    } else if (name === "jei" || name === "laiz") {
-      nomeVerificado = "marques voce e perfeita me";
+    if (name === "jeniffer" || name === "__marques15") {
+      nomeVerificado = "marques";
+    } else if (name === "alessandra" || name === "aylli.est") {
+        nomeVerificado = "ale";
     }
+
     return nomeVerificado;
 }
-//"validador"
-//"input"
-//"verificar"
-//"render"
+
+function renderDom(){
+    const mensagensObj = mensagens()
+    
+    divRender = cardMensagem(mensagensObj.msn1, mensagensObj.msn2, mensagensObj.msn3, mensagensObj.msn4);
+    
+    render.textContent = " "; 
+    render.appendChild(divRender)
+}
+
+function cardMensagem (msn1, msn2, msn3, msn4){
+    let divPai = document.createElement("div");
+    let div = document.createElement("div");
+
+    let mensagem1 = document.createElement("p");
+    let mensagem2 = document.createElement("p");
+    let mensagem3 = document.createElement("p");
+    let mensagem4 = document.createElement("p");
+
+    mensagem1.textContent = msn1;
+    mensagem2.textContent = msn2;
+    mensagem3.textContent = msn3;
+    mensagem4.textContent = msn4;
+
+    div.appendChild(mensagem1);
+    div.appendChild(mensagem2);
+    div.appendChild(mensagem3);
+    div.appendChild(mensagem4);
+
+    divPai.appendChild(div)
+
+    mensagem4.classList.add("text-underline")
+    divPai.classList.add("borde-render")
+    div.classList.add("box-render")
+    
+    return divPai;
+}
+
+function mensagens () {
+    const nome = verificarName(input.value.toLowerCase());
+    let mensagem1
+    let mensagem2
+    let mensagem3
+    let mensagem4
+
+    switch(nome){
+        case "marques" :
+            mensagem1 = "Queria te dar uma rosa, mas acho que seria pouco, então criei esse universo para você.";
+            mensagem2 = "Ao criar isso, percebi que a sua beleza supera todas as estrelas do universo.";
+            mensagem3 = "Espero que goste do seu universo; passei 3 dias para deixá-lo perfeito para você.";
+            mensagem4 = "Passando aqui para lembrar que você ainda vai ser minha.";
+            break;
+        case "ale" :
+            mensagem1 = "Queria te dar uma rosa, mas acho que seria pouco, então criei esse universo para você.";
+            mensagem2 = "E exaltar o quanto você é linda, perfeita.";
+            mensagem3 = "Passando aqui para lembrar que você ainda vai ser minha.";
+            break;
+        case undefined :
+            mensagem1 = "você não estar autorisado(a)"
+    }
+    return{
+       msn1: mensagem1,
+       msn2: mensagem2,
+       msn3: mensagem3,
+       msn4: mensagem4
+    }
+}
